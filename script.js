093cc33612acc2e999df70426233dfccd384b36a
thevijeeth@gmail.com <thevijeeth@gmail.com>
2024-04-21 08:00:50 +0530

    mn

---
+++ script.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const tooltipContainer = document.querySelector(".tooltip-container");
+  const tooltip = tooltipContainer.querySelector(".tooltip");
+
+  tooltipContainer.addEventListener("mouseenter", function() {
+    tooltip.style.visibility = "visible";
+  });
+
+  tooltipContainer.addEventListener("mouseleave", function() {
+    tooltip.style.visibility = "hidden";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const tooltipContainer2 = document.querySelector(".tooltip-container2");
+  const tooltip2 = tooltipContainer2.querySelector(".tooltip2");
+
+  tooltipContainer2.addEventListener("mouseenter", function() {
+    tooltip2.style.visibility = "visible";
+  });
+
+  tooltipContainer2.addEventListener("mouseleave", function() {
+    tooltip2.style.visibility = "hidden";
+  });
+
+  
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const tooltipContainer3 = document.querySelector(".tooltip-container3");
+  const tooltip3 = tooltipContainer3.querySelector(".tooltip3");
+
+  tooltipContainer3.addEventListener("mouseenter", function() {
+    tooltip3.style.visibility = "visible";
+  });
+
+  tooltipContainer3.addEventListener("mouseleave", function() {
+    tooltip3.style.visibility = "hidden";
+  });
+
+  
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const tooltipContainer4 = document.querySelector(".tooltip-container4");
+  const tooltip4 = tooltipContainer4.querySelector(".tooltip4");
+
+  tooltipContainer4.addEventListener("mouseenter", function() {
+    tooltip4.style.visibility = "visible";
+  });
+
+  tooltipContainer4.addEventListener("mouseleave", function() {
+    tooltip4.style.visibility = "hidden";
+  });
+
+  
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const tooltipContainer5 = document.querySelector(".tooltip-container5");
+  const tooltip5 = tooltipContainer5.querySelector(".tooltip5");
+
+  tooltipContainer5.addEventListener("mouseenter", function() {
+    tooltip5.style.visibility = "visible";
+  });
+
+  tooltipContainer5.addEventListener("mouseleave", function() {
+    tooltip5.style.visibility = "hidden";
+  });
+
+  
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const tooltipContainer6 = document.querySelector(".tooltip-container6");
+  const tooltip6 = tooltipContainer6.querySelector(".tooltip6");
+
+  tooltipContainer6.addEventListener("mouseenter", function() {
+    tooltip6.style.visibility = "visible";
+  });
+
+  tooltipContainer6.addEventListener("mouseleave", function() {
+    tooltip6.style.visibility = "hidden";
+  });
+
+  
+});
